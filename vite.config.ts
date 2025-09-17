01d54d0cba5436ddb753ed34fc1bc4de65e3f1a4
Alain Bindele <alain.bindele@gmail.com>
2025-09-17 10:46:03 +0200

    Add react-is as external dependency

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
+      external: ['react-is'],
       output: {
         manualChunks: {
           // Vendor chunks
